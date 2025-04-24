08d8284053471278db39b002e174c0456a407e98
Johndavid2004 <jhondavidgarciacapacho@gmail.com>
2025-04-23 23:18:08 -0500

    Delete Ejercicio-Formaci-n-Java-MVC---interfaces-POO/proyecto_interfaz directory

--- Ejercicio-Formaci-n-Java-MVC---interfaces-POO/proyecto_interfaz/src/Controlador/Proyecto_interfaz.java
+++
@@ -1,8 +0,0 @@
-package Controlador;
-
-public class Proyecto_interfaz {
-    public static void main(String[] args) {
-        // Iniciar el controlador (muestra las vistas automáticamente)
-        new Controlador();
-    }
-}
